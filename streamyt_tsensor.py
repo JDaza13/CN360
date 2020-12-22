1525bb1dca39e7a0862cdc30fa42855cad764dfe
JDaza13 <jdaza1327@gmail.com>
2020-12-22 13:50:48 -0500

    change log level on stream pipe

--- streamyt_tsensor.py
+++ streamyt_tsensor.py
@@ -64,7 +64,7 @@ config_logs()
 
 def main_stream():
 
-    stream_cmd = 'ffmpeg -re -ar 44100 -ac 2 -loglevel warning -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -thread_queue_size 64 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv ' + YOUTUBE + KEY 
+    stream_cmd = 'ffmpeg -re -ar 44100 -ac 2 -loglevel debug -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -thread_queue_size 64 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv ' + YOUTUBE + KEY 
     stream_pipe = subprocess.Popen(stream_cmd, shell=True, stdin=subprocess.PIPE) 
     camera = picamera.PiCamera(resolution=(H_SIZE, V_SIZE), framerate=FRAME_RATE)
     camera.annotate_background = picamera.Color('black')
