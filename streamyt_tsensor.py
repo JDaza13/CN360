27b4dd7348e49cc1a468cd5cf8898b549ba59127
JDaza13 <jdaza1327@gmail.com>
2021-01-07 14:24:38 -0500

    datetime counter fix

--- streamyt_tsensor.py
+++ streamyt_tsensor.py
@@ -27,7 +27,7 @@ TEMP_DEVICE_ID = '28-01193a3ed4e7'
 TEMP_DEVICE_PATH = '/sys/bus/w1/devices/'+TEMP_DEVICE_ID+'/w1_slave'
 TEMP_READ_FREQ_SEC = 10
 
-GENERAL_START_DATE = dt.strptime('24/09/20 00:00:01', '%d/%m/%y %H:%M:%S')
+GENERAL_START_DATE = dt.datetime.strptime('24/09/20 00:00:01', '%d/%m/%y %H:%M:%S')
 
 temp_val = 'temperature not available'
 
