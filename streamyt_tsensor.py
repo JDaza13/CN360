6ce8b02ac6d2e5c149b6961f2ff5ce7d8641eaa8
JDaza13 <jdaza1327@gmail.com>
2021-01-10 16:40:51 -0500

    Read soil moisture value from serial

--- streamyt_tsensor.py
+++ streamyt_tsensor.py
@@ -27,9 +27,13 @@ TEMP_DEVICE_ID = '28-01193a3ed4e7'
 TEMP_DEVICE_PATH = '/sys/bus/w1/devices/'+TEMP_DEVICE_ID+'/w1_slave'
 TEMP_READ_FREQ_SEC = 300
 
+SOIL_MOIST_SERIAL_NAME = '/dev/ttyUSB0'
+SOIL_MOIST_BAUD_RATE = 9600
+
 GENERAL_START_DATE = dt.datetime.strptime('24/09/20 00:00:01', '%d/%m/%y %H:%M:%S')
 
 temp_val = 'temperature not available'
+soil_moisture_value = 'soil moisture not available'
 
 def config_logs():
     global logger
@@ -61,7 +65,9 @@ config_logs()
 def main_stream():
 
     logger.warning('Starting stream at: ' + dt.datetime.now().strftime('%H:%M:%S'))
-    days_number = 0
+    days_number = 0    
+
+    serial_com = serial.Serial(SOIL_MOIST_SERIAL_NAME, SOIL_MOIST_BAUD_RATE, timeout=10)
 
     stream_cmd = 'ffmpeg -re -ar 44100 -ac 2 -loglevel warning -acodec pcm_s16le -f s16le -ac 2 -i /dev/zero -f h264 -thread_queue_size 64 -i - -vcodec copy -acodec aac -ab 128k -g 50 -strict experimental -f flv ' + YOUTUBE + KEY 
     stream_pipe = subprocess.Popen(stream_cmd, shell=True, stdin=subprocess.PIPE)
@@ -83,8 +89,16 @@ def main_stream():
                     executor.submit(get_temp, TEMP_DEVICE_PATH)
 
                 read_checkpoint = dt.datetime.now()
+                #read serial value
+                serial_com.flush()
+                if serial_com.in_waiting > 0:
+                    line = serial_com.readline().rstrip()
+                    line_value = re.findall('\d+', str(line))
+                    if line_value and len(line_value) > 0:
+                        soil_moisture_value = line_value[0]
+
             days_number = (time_now - GENERAL_START_DATE).days
-            camera.annotate_text = ' CN360 - Day ' + str(days_number) + ' \n ' + time_now.strftime('%Y-%m-%d %H:%M:%S') + ' \n ' + temp_val + ' '
+            camera.annotate_text = ' CN360 - Day ' + str(days_number) + ' \n ' + time_now.strftime('%Y-%m-%d %H:%M:%S') + ' \n ' + temp_val + ' ' + ' \n ' + soil_moisture_value + ' '
             camera.wait_recording(1)
     except Exception as ex:
         logger.warning(ex)
