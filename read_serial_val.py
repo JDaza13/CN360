589db150a901024b9f7664f3ce46011865e488f8
JDaza13 <jdaza1327@gmail.com>
2021-01-10 16:32:45 -0500

    Refactor v1

--- read_serial_val.py
+++ read_serial_val.py
@@ -3,11 +3,11 @@ import serial
 import re
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-    ser.flush()
+    serial_com = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    serial_com.flush()
     while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().rstrip()
+        if serial_com.in_waiting > 0:
+            line = serial_com.readline().rstrip()
             line_value = re.findall('\d+', str(line))
             if line_value and len(line_value) > 0:
             	print(line_value[0])
