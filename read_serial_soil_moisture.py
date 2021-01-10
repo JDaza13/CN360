509ec6e67a60021de6d998f0896e09de438f9ddc
JDaza13 <jdaza1327@gmail.com>
2021-01-10 17:14:04 -0500

    Parse to percentage

--- read_serial_soil_moisture.py
+++ read_serial_soil_moisture.py
@@ -4,11 +4,12 @@ import re
 
 soil_moisture_value = 'soil moisture unavailable'
 
-DRY_THRESHOLD = 0
-WET_THRESHOLD = 0
+DRY_THRESHOLD = 460
+WET_THRESHOLD = 215
 
 def parse_soil_moisture(serial_val):
-    return str((int(serial_val) - DRY_THRESHOLD) / (WET_THRESHOLD - DRY_THRESHOLD))
+	inverted_percentage = (int(serial_val) - WET_THRESHOLD) / (DRY_THRESHOLD - WET_THRESHOLD)
+    return str(100-inverted_percentage) + ' %'
 
 if __name__ == '__main__':
     serial_com = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
@@ -18,5 +19,5 @@ if __name__ == '__main__':
             line = serial_com.readline().rstrip()
             line_value = re.findall('\d+', str(line))
             if line_value and len(line_value) > 0:
-            	print(line_value[0])
-            	#print(parse_soil_moisture(line_value[0]))
+            	#print(line_value[0])
+            	print(parse_soil_moisture(line_value[0]))
