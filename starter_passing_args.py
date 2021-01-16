954a4e4a29ff2e1f2b9040c2384333a29a8ada75
JDaza13 <jdaza1327@gmail.com>
2021-01-16 12:38:31 -0500

    get arg on starter

--- starter_passing_args.py
+++ starter_passing_args.py
@@ -1,14 +1,20 @@
 from subprocess import run
 from time import sleep
 
-file_path = 'receive_args.py'
-run_args = ' 7ukg-59up-s1ya-4wdz-dvt0'
+import sys
 
-RESTART_WAIT_SECONDS = 3
+file_path = 'receive_args.py'
+run_arg = ''
+
+def get_arg_from_cla(argv):
+	global run_arg
+	if len(argv) > 1:
+		run_arg = str(argv[1])
 
 def start_script():
+	get_arg_from_cla(sys.argv)
     try:
-        run('python3 ' + file_path + run_args, shell=True, check=True) 
+        run('python3 ' + file_path + ' ' + run_arg, shell=True, check=True) 
     except Exception as ex:
         print('Child script crashed')
 
