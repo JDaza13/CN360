eb9dacb8b55f9f71d579c6da9cc2ef0e5bf0e4e6
JDaza13 <jdaza1312@gmail.com>
2021-05-29 12:25:05 -0500

    add encoding

--- write_serial_val.py
+++ write_serial_val.py
@@ -9,10 +9,10 @@ if __name__ == '__main__':
     serial_com = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
     serial_com.flush()
     while True:
-        if light_control_state == "on":
-            light_control_state = "off"
-        if light_control_state == "off":
-            light_control_state = "on"
+        if light_control_state == 'on':
+            light_control_state = 'off'
+        if light_control_state == 'off':
+            light_control_state = 'on'
         #light control
-        serial_com.write(light_control_state + '\n');
+        serial_com.write((light_control_state + '\n').encode());
         time.sleep(5)
